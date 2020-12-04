42555dd6e32bf685dacdd4028f1d64323be4ad59
edgarrii <71753826+edgarrii@users.noreply.github.com>
2020-12-04 12:46:08 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,231 @@
+#include <iostream>
+#include <string>
+#include <Windows.h>
+#include <fstream>
+#include <conio.h>
+#include <iomanip>
+
+using namespace std;
+
+int writeNumbers();
+int chooseMainMenu();
+int chooseEquation(string*, int);
+
+template <typename T>
+class Equation {
+private:
+	T a, b, c;
+	T D;
+	T x1;
+	T x2;
+public:
+	Equation() {
+		a = b = c = x1 = x2 = D = 0;
+	}
+	Equation(T a1, T b1, T c1) {
+		a = a1;
+		b = b1;
+		c = c1;
+		D = b * b - 4 * a * c;
+		x1 = (-b + sqrt(D)) / (2 * a);
+		x2 = (-b - sqrt(D)) / (2 * a);
+	}
+	void show() {
+		D = b * b - 4 * a * c;
+		x1 = (-b + sqrt(D)) / (2 * a);
+		x2 = (-b - sqrt(D)) / (2 * a);
+		cout << a << "x^2";
+		if (b < 0) cout << b << "x";
+		else cout << "+" << b << "x";
+		if (c < 0) cout << c;
+		else cout << "+" << c;
+		cout << endl;
+		cout << "D = b^2-4ac" << endl;
+		T rav = 4 * a * c;
+		if (rav < 0) cout << "D = " << b * b << "+" << -rav;
+		else cout << "D = " << b * b << "-" << rav;
+		cout << " = " << D << endl;
+		cout << "x1,2 = (-b + sqrt(D))/2a" << endl;
+		cout << "x1 = " << x1 << endl;
+		cout << "x2 = " << x2 << endl;
+	}
+	Equation<T>& operator*=(int abc) {
+		a *= abc;
+		b *= abc;
+		c *= abc;
+		return*this;
+	}
+	Equation<T>& operator-(Equation<T>& ur) {
+		a = a - ur.a;
+		b = b - ur.b;
+		c = c - ur.c;
+		return*this;
+	}
+	string getUrav() {
+		string s;
+		s = to_string(a) + "x^2";
+		if (b < 0) s = s + to_string(b) + "x";
+		else s = s + "+" + to_string(b) + "x";
+		if (c < 0) s = s + to_string(c);
+		else s = s + "+" + to_string(c);
+		return s;
+	}
+	friend void operator>><T>(istream&, Equation<T>&);
+};
+
+
+template <typename T> void operator>>(istream& in, Equation<T>& ur) {
+	cout << "Введите коэффицент a: ";
+	ur.a = writeNumbers();
+	system("cls");
+	cout << "Введите коэффицент b: ";
+	ur.b = writeNumbers();
+	system("cls");
+	cout << "Введите коэффицент c: ";
+	ur.c = writeNumbers();
+	system("cls");
+}
+
+
+int main() {
+	setlocale(0, "rus");
+	int nEquation = 0;
+	Equation<double>* equation = new Equation<double>[1];
+	while (true) {
+		int choose = chooseMainMenu();
+		if (choose == 0) {
+			nEquation++;
+			Equation<double>* ptr = new Equation<double>[nEquation];
+			for (int i = 0; i < nEquation - 1; i++)
+				ptr[i] = equation[i];
+			equation = ptr;
+			ptr = NULL;
+			delete[]ptr;
+			cin >> equation[nEquation - 1];
+		}
+		else
+			if (choose == 1) {
+				for (int i = 0; i < nEquation; i++) {
+					equation[i].show();
+					cout << endl;
+				}
+				system("pause");
+				system("cls");
+			}
+			else
+				if (choose == 2) {
+					string* aa = new string[nEquation];
+					for (int i = 0; i < nEquation; i++)
+						aa[i] = equation[i].getUrav();
+					int choose1 = chooseEquation(aa, nEquation);
+					delete[]aa;
+					if (choose1 == nEquation) break;
+					cout << "Введите число на которое умножить коэффиценты: ";
+					int a = writeNumbers();
+					equation[choose1] *= a;
+				}
+				else
+					if (choose == 3) {
+						string* aa = new string[nEquation];
+						for (int i = 0; i < nEquation; i++)
+							aa[i] = equation[i].getUrav();
+						int choose1 = chooseEquation(aa, nEquation);
+						if (choose1 == nEquation) break;
+						int choose2 = chooseEquation(aa, nEquation);
+						if (choose2 == nEquation) break;
+						equation[choose1] = equation[choose1] - equation[choose2];
+						delete[]aa;
+						cout << "Вычетание прошло успешно!" << endl;
+						system("pause");
+						system("cls");
+					}
+					else
+						break;
+	}
+}
+
+int chooseMainMenu() {
+	int choose_menu = 0, key = 0;
+	system("cls");
+	while (true) {
+		choose_menu = (choose_menu + 5) % 5;
+
+		if (choose_menu == 0) cout << "-> Добавить уравнение" << endl;
+		else cout << " Добавить уравнение" << endl;
+
+		if (choose_menu == 1) cout << "-> Посмотреть уравнения и решить" << endl;
+		else cout << " Посмотреть уравнения и решить" << endl;
+
+		if (choose_menu == 2) cout << " -> Умножить коэфиценты на число" << endl;
+		else cout << " Умножить коэфиценты на число" << endl;
+
+		if (choose_menu == 3) cout << " -> Вычесть коэфиценты одного уравнение от другого" << endl;
+		else cout << " Вычесть коэфиценты одного уравнение от другого" << endl;
+
+		if (choose_menu == 4) cout << " -> Выход" << endl;
+		else cout << " Выход" << endl;
+
+		key = _getch();
+		if (key == 224) {
+			key = _getch();
+			if (key == 72) choose_menu--;
+			if (key == 80) choose_menu++;
+		}
+		if (key == 13) {
+			system("cls");
+			return choose_menu;
+		}
+		system("cls");
+	}
+}
+
+int writeNumbers() {
+	string numbers;
+	while (true) {
+		int key;
+		key = _getch();
+		if (key == 224) {
+			key = _getch();
+		}
+		else
+			if (key == 8) {
+				if (numbers.length() != 0) {
+					cout << '\b' << " " << '\b';
+					numbers.erase(numbers.length() - 1);
+				}
+			}
+			else
+				if (key == 13 && numbers.length() != 0 && numbers != "-") break;
+				else
+					if ((key >= '0' && key <= '9') || (key == '-' && numbers.length() == 0)) {
+						numbers = numbers + (char)key;
+						cout << (char)key;
+					}
+	}
+	cout << endl;
+	return stoi(numbers);
+}
+
+int chooseEquation(string* a, int nEquation) {
+	int choose_menu = 0, keyboard_button = 0;
+	while (true) {
+		cout << "Стрелочками ВВЕРХ и ВНИЗ выберите пункт меню" << endl;
+		choose_menu = (choose_menu + (nEquation + 1)) % (nEquation + 1);
+
+		for (int i = 0; i < nEquation; i++)
+			if (i == choose_menu) { cout << " -> " << i + 1 << " - " << a[i] << endl; }
+			else { cout << " " << i + 1 << " - " << a[i] << endl; }
+
+		if (choose_menu == nEquation) { cout << " -> Выйти с выбора" << endl; }
+		else cout << " Выйти с выбора" << endl;
+
+		keyboard_button = _getch();
+		if (keyboard_button == 224) {
+			keyboard_button = _getch();
+			if (keyboard_button == 72) choose_menu--;
+			if (keyboard_button == 80) choose_menu++;
+		}
+		if (keyboard_button == 13) { system("cls"); return choose_menu; }
+		system("cls");
+	}
+}
--- main.cpp
+++ main.cpp
@@ -1,85 +1,426 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+#include <Windows.h>
+#include <fstream>
+#include <conio.h>
+#include <iomanip>
 
 using namespace std;
 
-int main()
-{
-    setlocale(LC_ALL, "rus");
-
-    cout << "Enter the size of the array, press ENTER and enter the array itself:" << endl;
-	int n;
-	cin >> n;
-
-	//received the matrix size
-	int** a = new int*[n];
-	for (int i = 0; i < n; i++)
-	{
-		a[i] = new int[n];
+class Voleyball {
+protected:
+	string name_team;
+	string country;
+public:
+	Voleyball() {};
+	Voleyball(string w1, string w2) {
+		name_team = w1;
+		country = w2;
 	}
-	int el = 0;
-	//has allocated a place for the matrix and the element you are looking for
-	for (int i = 0; i < n; i++)
-	{
-		for (int k = 0; k < n; k++)
-		{
-			cin >> a[i][k];
-			if (abs(a[i][k]) > el)
-				el = abs(a[i][k]);
-		}
+	void changeNameTeam(string w) {
+		name_team = w;
 	}
-	//got an array and max element by module
-	int xd, yd;
-	bool flag = false;
-	//coordinates of the searched number, flag proverki poiska
-	for (int i = 0; i < n; i++)
-	{
-		for (int k = 0; k < n; k++)
-		{
-			if (a[i][k] == el)
-			{
-				flag = true;
-				yd = i;
-				xd = k;
-				break;
-			}
-		}
-		if (flag == true)
-			break;
+	void changeCountry(string w) {
+		country = w;
 	}
-
-	cout << "\n";
-
-	if (flag == false)
-	{
-		cout << "Integer is not found";
-		return 0;
+	string getNameTeam() {
+		return name_team;
 	}
+	string getCountry() {
+		return country;
+	}
+	void show() {
+		cout << "Страна команды: " << country << endl;
+		cout << "Название команды: " << name_team << endl;
+	}
+};
 
-	for (int i = 0; i < n; i++)
-	{
-		if (yd != i)
-		{
-			for (int k = 0; k < n; k++)
-			{
-				if (xd != k)
-				{
-					cout << a[i][k] << " ";
+class Team :virtual public Voleyball {
+protected:
+	int howManyPlayers;
+	int howManyWins;
+public:
+	Team() {
+		howManyPlayers = 0;
+		howManyWins = 0;
+	};
+	Team(int w, int w1, string w2, string w3) :Voleyball(w2, w2) {
+		howManyPlayers = w;
+		howManyWins = w1;
+	}
+	void changeHowManyPlayers(int w) {
+		howManyPlayers = w;
+	}
+	void changeHowManyWins(int w) {
+		howManyWins = w;
+	}
+	int getHowManyPlayers() {
+		return howManyPlayers;
+	}
+	int getHowManyWinsy() {
+		return howManyWins;
+	}
+	void show() {
+		cout << "Кол-во игроков в команде: " << howManyPlayers << endl;
+		cout << "Кол-во выигранных матчей: " << howManyWins << endl;
+	}
+};
+
+class Coach :virtual public Voleyball {
+protected:
+	string name;
+	string s_name;
+public:
+	Coach() {};
+	Coach(string w, string w1, string w2, string w3) :Voleyball(w2, w3) {
+		name = w;
+		s_name = w1;
+	}
+	void changeName(string w) {
+		name = w;
+	}
+	void changeSName(string w) {
+		s_name = w;
+	}
+	string getName() {
+		return name;
+	}
+	string getSName() {
+		return s_name;
+	}
+	void show() {
+		cout << "Имя тренера: " << name << endl;
+		cout << "Фамилия тренера: " << s_name << endl;
+	}
+};
+
+class Player :public Team, public Coach {
+private:
+	string name_player;
+	string s_name_player;
+public:
+	Player() {};
+	Player(string w, string w1, string w2, string w3, int w4, int w5, string w6, string w7) :Voleyball(w2, w3), Team(w4, w5, w2, w3), Coach(w6, w7, w2, w3) {
+		name = w;
+		s_name = w1;
+	}
+	void changeNamePlayer(string w) {
+		name_player = w;
+	}
+	void changeSNamePlayer(string w) {
+		s_name_player = w;
+	}
+	string getNamePlayer() {
+		return name_player;
+	}
+	string getSNamePlayer() {
+		return s_name_player;
+	}
+	void show() {
+		Voleyball::show();
+		Team::show();
+		Coach::show();
+		cout << "Имя игрока: " << name_player << endl;
+		cout << "Фамилия игрока: " << s_name_player << endl;
+	}
+};
+
+int chooseMenu();
+string writeWords();
+int writeNumbers();
+int chooseWhoRed(Player*&, int);
+int chooseWhatRed(Player*&, int);
+
+int main() {
+	setlocale(0, "rus");
+	int nPlayer = 0;
+	Player* players = new Player[nPlayer];
+	while (true) {
+		int choose = chooseMenu();
+		if (choose == 1) {
+			if (nPlayer != 0) {
+				while (true) {
+					int choose1 = chooseWhoRed(players, nPlayer);
+					if (choose1 == nPlayer) break;
+					while (true) {
+						int choose2 = chooseWhatRed(players, choose1);
+						if (choose2 == 8) break;
+						string ptr1; int ptr;
+						if (choose2 == 0) {
+							cout << "Введите название команды: ";
+							ptr1 = writeWords();
+							players[choose1].changeNameTeam(ptr1);
+							system("cls");
+						}
+						else
+							if (choose2 == 1) {
+								cout << "Введите страну команды: ";
+								ptr1 = writeWords();
+								players[choose1].changeCountry(ptr1);
+								system("cls");
+							}
+							else
+								if (choose2 == 2) {
+									cout << "Введите кол-во игроков: ";
+									int ptr = writeNumbers();
+									players[choose1].changeHowManyPlayers(ptr);
+									system("cls");
+								}
+								else
+									if (choose2 == 3) {
+										cout << "Введите кол-во выигранных матчей: ";
+										ptr = writeNumbers();
+										players[choose1].changeHowManyWins(ptr);
+										system("cls");
+									}
+									else
+										if (choose2 == 4) {
+											cout << "Введите имя тренера: ";
+											ptr1 = writeWords();
+											players[choose1].changeName(ptr1);
+											system("cls");
+										}
+										else
+											if (choose2 == 5) {
+												cout << "Введите фамилию тренера: ";
+												ptr1 = writeWords();
+												players[choose1].changeSName(ptr1);
+												system("cls");
+											}
+											else
+												if (choose2 == 6) {
+													cout << "Введите имя игрока: ";
+													ptr1 = writeWords();
+													players[choose1].changeNamePlayer(ptr1);
+													system("cls");
+												}
+												else
+													if (choose2 == 7) {
+														cout << "Введите фамилию игрока: ";
+														ptr1 = writeWords();
+														players[choose1].changeSNamePlayer(ptr1);
+														system("cls");
+													}
+					}
 				}
 			}
-			cout << "\n";
+			else {
+				cout << "Вы еще не добавили историй" << endl;
+				system("pause");
+				system("cls");
+			}
 		}
+		else
+			if (choose == 0) {
+				Player sum;
+				cout << "Введите название команды: ";
+				string ptr1 = writeWords();
+				sum.changeNameTeam(ptr1);
+				system("cls");
+				cout << "Введите страну команды: ";
+				ptr1 = writeWords();
+				sum.changeCountry(ptr1);
+				system("cls");
+				cout << "Введите кол-во игроков: ";
+				int ptr = writeNumbers();
+				sum.changeHowManyPlayers(ptr);
+				system("cls");
+				cout << "Введите кол-во выигранных матчей: ";
+				ptr = writeNumbers();
+				sum.changeHowManyWins(ptr);
+				system("cls");
+				cout << "Введите имя тренера: ";
+				ptr1 = writeWords();
+				sum.changeName(ptr1);
+				system("cls");
+				cout << "Введите фамилию тренера: ";
+				ptr1 = writeWords();
+				sum.changeSName(ptr1);
+				system("cls");
+				cout << "Введите имя игрока: ";
+				ptr1 = writeWords();
+				sum.changeNamePlayer(ptr1);
+				system("cls");
+				cout << "Введите фамилию игрока: ";
+				ptr1 = writeWords();
+				sum.changeSNamePlayer(ptr1);
+				system("cls");
+				nPlayer++;
+				Player* ptrr = new Player[nPlayer];
+				for (int i = 0; i < nPlayer - 1; i++)
+					ptrr[i] = players[i];
+				players = ptrr;
+				ptrr = NULL;
+				delete[]ptrr;
+				players[nPlayer - 1] = sum;
+			}
+			else
+				if (choose == 2) {
+					for (int i = 0; i < nPlayer; i++) {
+						players[i].show();
+						cout << endl;
+						cout << endl;
+					}
+					system("pause");
+					system("cls");
+				}
+				else
+					if (choose == 3) break;
 	}
-
-	cout << "\n";
-
-
-
-	for (int i = 0; i < n; i++)
-	{
-		delete[] a[i];
-	}
-	delete[] a;
-	//relieved the memory
-
 	return 0;
+}
+
+string writeWords() {
+	string slovo;
+	while (true) {
+		int Knopka;
+		char sumbol;
+		Knopka = _getch();
+		if (Knopka == 224) {
+			Knopka = _getch();
+		}
+		else
+			if (Knopka == 8) {
+				if (slovo.length() != 0) {
+					cout << '\b' << " " << '\b';
+					slovo.erase(slovo.length() - 1);
+				}
+			}
+			else
+				if (Knopka == 13 && slovo.length() != 0) {
+					break;
+				}
+				else {
+					sumbol = (char)Knopka;
+					if ((sumbol >= 'a' && sumbol <= 'z') || (sumbol >= 'A' && sumbol <= 'Z') || (sumbol == ' ')) {
+						slovo = slovo + sumbol;
+						cout << sumbol;
+					}
+				}
+	}
+	return slovo;
+}
+
+int writeNumbers() {
+	string numbers;
+	while (true) {
+		int key;
+		key = _getch();
+		if (key == 224) {
+			key = _getch();
+		}
+		else
+			if (key == 8) {
+				if (numbers.length() != 0) {
+					cout << '\b' << " " << '\b';
+					numbers.erase(numbers.length() - 1);
+				}
+			}
+			else
+				if (key == 13 && numbers.length() != 0) break;
+				else
+					if (key >= '0' && key <= '9') {
+						numbers = numbers + (char)key;
+						cout << (char)key;
+					}
+	}
+	return stoi(numbers);
+}
+
+int chooseMenu() {
+	int choose_menu = 0, keyboard_button = 0;
+	while (true) {
+		cout << "Используя стрелочки Up и Down выберите необходимый пункт меню: " << endl;
+		choose_menu = (choose_menu + 4) % 4;
+
+		if (choose_menu == 0) { cout << " -> Добавить игрока" << endl; }
+		else { cout << " Добавить игрока" << endl; }
+
+		if (choose_menu == 1) { cout << " -> Редактировать игроков" << endl; }
+		else { cout << " Редактировать игроков" << endl; }
+
+		if (choose_menu == 2) { cout << " -> Просмотр всех игроков" << endl; }
+		else { cout << " Просмотр всех игроков" << endl; }
+
+		if (choose_menu == 3) { cout << " -> Завершить программу" << endl; }
+		else { cout << " Завершить программу" << endl; }
+
+		keyboard_button = _getch();
+		if (keyboard_button == 224) {
+			keyboard_button = _getch();
+			if (keyboard_button == 72) choose_menu--;
+			if (keyboard_button == 80) choose_menu++;
+		}
+		if (keyboard_button == 13) { system("cls"); return choose_menu; }
+		system("cls");
+	}
+}
+
+int chooseWhoRed(Player*& players, int nPlayer) {
+	int choose_menu = 0, keyboard_button = 0;
+	while (true) {
+		cout << "Стрелочками ВВЕРХ и ВНИЗ выберите пункт меню" << endl;
+		choose_menu = (choose_menu + (nPlayer + 1)) % (nPlayer + 1);
+
+		for (int i = 0; i < nPlayer; i++)
+			if (i == choose_menu) { cout << " -> " << i + 1 << " - " << players[i].getName() << endl; }
+			else { cout << " " << i + 1 << " - " << players[i].getName() << endl; }
+
+		if (choose_menu == nPlayer) { cout << " -> Выйти с выбора игрока" << endl; }
+		else cout << " Выйти с выбора игрока" << endl;
+
+		keyboard_button = _getch();
+		if (keyboard_button == 224) {
+			keyboard_button = _getch();
+			if (keyboard_button == 72) choose_menu--;
+			if (keyboard_button == 80) choose_menu++;
+		}
+		if (keyboard_button == 13) { system("cls"); return choose_menu; }
+		system("cls");
+	}
+}
+
+int chooseWhatRed(Player*& players, int choose) {
+	int choose_menu = 0, keyboard_button = 0;
+	while (true) {
+		cout << "Стрелочками ВВЕРХ и ВНИЗ выберите пункт меню" << endl;
+		choose_menu = (choose_menu + 9) % 9;
+
+		if (choose_menu == 0) { cout << " -> Название команды: " << players[choose].getNameTeam() << endl; }
+		else cout << " Название команды: " << players[choose].getNameTeam() << endl;
+
+		if (choose_menu == 1) { cout << " -> Страна: " << players[choose].getCountry() << endl; }
+		else cout << " Тип Страна: " << players[choose].getCountry() << endl;
+
+		if (choose_menu == 2) { cout << " -> Кол-во игроков: " << players[choose].getHowManyPlayers() << endl; }
+		else cout << " Кол-во игроков: " << players[choose].getHowManyPlayers() << endl;
+
+		if (choose_menu == 3) { cout << " -> Кол-во выигранных матчей: " << players[choose].getHowManyWinsy() << endl; }
+		else cout << " Кол-во выигранных матчей: " << players[choose].getHowManyWinsy() << endl;
+
+		if (choose_menu == 4) { cout << " -> Имя тренера: " << players[choose].getName() << endl; }
+		else cout << " Имя тренера: " << players[choose].getName() << endl;
+
+		if (choose_menu == 5) { cout << " -> Фамилия тренера: " << players[choose].getSName() << endl; }
+		else cout << " Фамилия тренера: " << players[choose].getSName() << endl;
+
+		if (choose_menu == 6) { cout << " -> Имя игрока: " << players[choose].getNamePlayer() << endl; }
+		else cout << " Имя игрока: " << players[choose].getNamePlayer() << endl;
+
+		if (choose_menu == 7) { cout << " -> Фамилия игрока: " << players[choose].getSNamePlayer() << endl; }
+		else cout << " Фамилия игрока: " << players[choose].getSNamePlayer() << endl;
+
+		if (choose_menu == 8) { cout << " -> Выйти в меню выбора " << endl; }
+		else cout << " Выйти в меню выбора " << endl;
+
+
+		keyboard_button = _getch();
+		if (keyboard_button == 224) {
+			keyboard_button = _getch();
+			if (keyboard_button == 72) choose_menu--;
+			if (keyboard_button == 80) choose_menu++;
+		}
+		if (keyboard_button == 13) { system("cls"); return choose_menu; }
+		system("cls");
+	}
 }
